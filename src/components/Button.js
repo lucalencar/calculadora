11ecb9f3923567bc16b22569ba4d11c44b8f3f4b
lucalencar <54904248+lucalencar@users.noreply.github.com>
2022-01-22 10:43:33 -0300

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ export default function Button({ valor, getValor }) {
 
     return (
         <div>
-            <S.Botao onClick={(e) => getValor(valor)}>{valor}</S.Botao>
+            <S.Botao onClick={() => getValor(valor)}>{valor}</S.Botao>
         </div>
     );
 
